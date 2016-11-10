aee6b5a461520471d2d94abf78a6c5d2c72a2e14
White-CHN <450756957@qq.com>
2016-11-10 13:01:20 +0800

    Add files via upload

--- DuiDemo/DemoFrame.cpp
+++ DuiDemo/DemoFrame.cpp
@@ -13,36 +13,37 @@ CDemoFrame::~CDemoFrame(void)
 
 DuiLib::CDuiString CDemoFrame::GetSkinFile()
 {
-	return _T("main.xml");
+    return _T("main.xml");
 }
 
 LPCTSTR CDemoFrame::GetWindowClassName() const
 {
-	return _T("MainWnd");
+    return _T("MainWnd");
 }
 
 LRESULT CDemoFrame::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	return __super::HandleMessage(uMsg, wParam, lParam);
+    return __super::HandleMessage(uMsg, wParam, lParam);
 }
 
 void CDemoFrame::OnFinalMessage(HWND hWnd)
 {
-
 }
 
 LRESULT CDemoFrame::MessageHandler(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
 {
-	return 0;
+    return 0;
 }
 
 CDuiControl* CDemoFrame::CreateControl(LPCTSTR pstrClass)
 {
-	return NULL;
+    return NULL;
 }
 
 void CDemoFrame::Notify(TNotifyUI& msg)
 {
-
+    if(msg.sType == _T("windowinit"))
+    {
+    }
 }
 
--- DuiDemo/DuiDemo.cpp
+++ DuiDemo/DuiDemo.cpp
@@ -6,47 +6,47 @@
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int nCmdShow)
 {
-	HRESULT hRes = ::CoInitialize(NULL);
-	if( FAILED(hRes) ) return 0;
-	hRes = ::OleInitialize(NULL);
-	if( FAILED(hRes) ) return 0;
-	
-	CDuiConsole Console ;// 调试信息控制台
-	// 初始化UI管理器
-	CDuiPaintManager::Initialize(hInstance);
-
-	// 资源类型
+    HRESULT hRes = ::CoInitialize(NULL);
+    if(FAILED(hRes))
+    {
+        return 0;
+    }
+    hRes = ::OleInitialize(NULL);
+    if(FAILED(hRes))
+    {
+        return 0;
+    }
+    CDuiConsole Console ;// 调试信息控制台
+    // 初始化UI管理器
+    CDuiPaintManager::Initialize(hInstance);
+    // 资源类型
 #ifdef _DEBUG
-	CDuiPaintManager::SetResourceType(DUILIB_FILE);
+    CDuiPaintManager::SetResourceType(DUILIB_FILE);
 #else
-	CDuiPaintManager::SetResourceType(DUILIB_ZIPRESOURCE);
+    CDuiPaintManager::SetResourceType(DUILIB_ZIPRESOURCE);
 #endif
-	// 资源路径
-	CDuiString strResourcePath = CDuiPaintManager::GetInstancePath();
-	// 加载资源
-	strResourcePath += _T("skin\\");
-	CDuiPaintManager::SetResourcePath(strResourcePath.GetData());
-
-	switch(CDuiPaintManager::GetResourceType())
-	{
-	case DUILIB_FILE:
-		{
-
-			// 加载资源管理器
-			//CResourceManager::GetInstance()->LoadResource(_T("res.xml"), NULL);
-			break;
-		}
-	}
-	CDemoFrame *pDemoFrame = new CDemoFrame;
-	pDemoFrame->Create(NULL, _T("duilib使用例子集锦（By White）"), UI_WNDSTYLE_FRAME, 0L, 0, 0, 800, 572);
-	pDemoFrame->CenterWindow();
-	CDuiPaintManager::MessageLoop();
-
-	DUI_FREE_POINT(pDemoFrame);
-	CDuiPaintManager::Uninitialize();
-
-	OleUninitialize();
-	::CoUninitialize();
-	return 0;
+    // 资源路径
+    CDuiString strResourcePath = CDuiPaintManager::GetInstancePath();
+    // 加载资源
+    strResourcePath += _T("skin\\");
+    CDuiPaintManager::SetResourcePath(strResourcePath.GetData());
+    switch(CDuiPaintManager::GetResourceType())
+    {
+        case DUILIB_FILE:
+        {
+            // 加载资源管理器
+            //CResourceManager::GetInstance()->LoadResource(_T("res.xml"), NULL);
+            break;
+        }
+    }
+    CDemoFrame* pDemoFrame = new CDemoFrame;
+    pDemoFrame->Create(NULL, _T("duilib使用例子集锦（By White）"), UI_WNDSTYLE_FRAME, 0L, 0, 0, 800, 572);
+    pDemoFrame->CenterWindow();
+    CDuiPaintManager::MessageLoop();
+    DUI_FREE_POINT(pDemoFrame);
+    CDuiPaintManager::Uninitialize();
+    OleUninitialize();
+    ::CoUninitialize();
+    return 0;
 }
 
